6aae2cdf213f8e7841fe1d91e2ebb6e9dc6f849f
crashtheparty <laytfire2@gmail.com>
2019-02-17 22:34:28 -0700

    Release 1.4.1

---
+++ src/org/ctp/enchantmentsolution/listeners/chestloot/ChestLootException.java
@@ -0,0 +1,14 @@
+package org.ctp.enchantmentsolution.listeners.chestloot;
+
+public class ChestLootException extends RuntimeException{
+
+	public ChestLootException(String string, Throwable throwable) {
+		super(string, throwable);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
