6758396118edf1d7b7dcce13f083e928b5486de7
crashtheparty <laytfire2@gmail.com>
2019-07-29 00:02:18 -0600

    Final changes for 2.1.3

--- src/org/ctp/enchantmentsolution/enchantments/helper/UpdateItem.java
+++ src/org/ctp/enchantmentsolution/enchantments/helper/UpdateItem.java
@@ -53,15 +53,15 @@ public class UpdateItem {
 		
 		if(repairType.equals(RepairType.REPAIR)) {
 			int amount = itemTwo.getAmount();
-			int durPerItem = itemOne.getType().getMaxDurability() / 4;
-			while(DamageUtils.getDamage(itemOne.getItemMeta()) > 0 && amount > 0) {
+			int durPerItem = combinedItem.getType().getMaxDurability() / 4;
+			while(DamageUtils.getDamage(combinedItem.getItemMeta()) > 0 && amount > 0) {
 				amount--;
 				repairCost++;
-				DamageUtils.setDamage(itemOne, (DamageUtils.getDamage(itemOne.getItemMeta()) - durPerItem));
+				DamageUtils.setDamage(combinedItem, (DamageUtils.getDamage(combinedItem.getItemMeta()) - durPerItem));
 			}
 			
-			if(DamageUtils.getDamage(itemOne.getItemMeta()) < 0) {
-				DamageUtils.setDamage(itemOne, 0);
+			if(DamageUtils.getDamage(combinedItem.getItemMeta()) < 0) {
+				DamageUtils.setDamage(combinedItem, 0);
 			}
 			if(amount > 0) {
 				itemTwoLeftover = itemTwo.clone();
@@ -85,7 +85,9 @@ public class UpdateItem {
 		}else {
 			combinedItem = AnvilNMS.setRepairCost(combinedItem, itemTwoRepair * 2 + 1);
 		}
-		combinedItem = Enchantments.addEnchantmentsToItem(combinedItem, enchantments.getEnchantments());
+		if(enchantments != null) {
+			combinedItem = Enchantments.addEnchantmentsToItem(combinedItem, enchantments.getEnchantments());
+		}
 		repairCost += itemOneRepair + itemTwoRepair;
 		
 		repairCost = Math.max(repairCost / ConfigUtils.getLevelDivisor(), 1);
