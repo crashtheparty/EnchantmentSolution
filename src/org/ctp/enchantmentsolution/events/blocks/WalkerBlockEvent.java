332128956cbae398700c8113864ce8060ad57c02
crashtheparty <laytfire2@gmail.com>
2020-04-05 17:30:23 -0600

    Stop the WalkerBlockEvent from having null values

--- src/org/ctp/enchantmentsolution/events/blocks/WalkerBlockEvent.java
+++ src/org/ctp/enchantmentsolution/events/blocks/WalkerBlockEvent.java
@@ -1,16 +1,19 @@
 package org.ctp.enchantmentsolution.events.blocks;
 
+import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 import org.ctp.enchantmentsolution.enchantments.helper.EnchantmentLevel;
 
 public abstract class WalkerBlockEvent extends ESBlockPlaceEvent {
 
 	public WalkerBlockEvent(Block placedBlock, BlockState replacedBlockState, Player thePlayer,
 	EnchantmentLevel enchantment) {
-		super(placedBlock, replacedBlockState, null, null, thePlayer, true, EquipmentSlot.FEET, enchantment);
+		super(placedBlock, replacedBlockState, placedBlock.getRelative(BlockFace.DOWN), new ItemStack(Material.AIR), thePlayer, true, EquipmentSlot.FEET, enchantment);
 	}
 
 }
