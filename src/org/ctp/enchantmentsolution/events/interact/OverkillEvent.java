ce0634c3fe5589abebdf45f278af33233d4a6706
crashtheparty <laytfire2@gmail.com>
2020-06-24 16:48:44 -0600

    Modify the SoulItems and add ways to check for a cooldown

--- src/org/ctp/enchantmentsolution/events/interact/OverkillEvent.java
+++ src/org/ctp/enchantmentsolution/events/interact/OverkillEvent.java
@@ -12,7 +12,7 @@ public class OverkillEvent extends ProjectileSpawnEvent {
 	private double speed;
 
 	public OverkillEvent(Player who, ItemStack item, boolean takeArrow, boolean hasArrow, double speed) {
-		super(who, new EnchantmentLevel(CERegister.OVERKILL, 1), item);
+		super(who, new EnchantmentLevel(CERegister.OVERKILL, 1), item, 2);
 		setTakeArrow(takeArrow);
 		this.hasArrow = hasArrow;
 		setSpeed(speed);
@@ -40,7 +40,7 @@ public class OverkillEvent extends ProjectileSpawnEvent {
 
 	@Override
 	public boolean willCancel() {
-		return !hasArrow && takeArrow;
+		return !hasArrow && takeArrow || super.willCancel();
 	}
 
 }
